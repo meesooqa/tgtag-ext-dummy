e8574d797a8d471be74e1f25736b981b35359246
meesooqa <meesooqa@hotmail.com>
2025-02-27 18:36:40 +0300

    add static #1

--- ext/dummy_ext/extension.go
+++ ext/dummy_ext/extension.go
@@ -1,10 +1,18 @@
 package dummy_ext
 
 import (
+	"embed"
+
 	"github.com/meesooqa/tgtag/pkg/controllers"
 	"github.com/meesooqa/tgtag/pkg/extensions"
 	"github.com/meesooqa/tgtag/pkg/repositories"
 )
+
+//go:embed template/content/*.html
+var fsContentTpl embed.FS
+
+//go:embed template/static
+var fsStaticDir embed.FS
 
 type DummyExtension struct {
 	extensions.BaseExtension
@@ -12,6 +20,9 @@ type DummyExtension struct {
 
 func NewDummyExtension(repo repositories.Repository) *DummyExtension {
 	return &DummyExtension{extensions.BaseExtension{
+		Name:         "dummy_ext",
+		FsContentTpl: fsContentTpl,
+		FsStaticDir:  fsStaticDir,
 		Controllers: []controllers.Controller{
 			NewDummyController(repo),
 		},
